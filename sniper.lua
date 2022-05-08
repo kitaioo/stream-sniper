c04299c444fc0885c7e2ba4c6e1fd335d11e9d82
ScreamBirb <59750253+ScreamBirb@users.noreply.github.com>
2022-05-08 14:35:42 -0230

    fixed.

--- sniper.lua
+++ sniper.lua
@@ -1,5 +1,5 @@
-local user_id = "673475"
-local game_id = "6361937392"
+local user_id = "3515195953"
+local game_id = "3976767347"
 
 local http_service = game:GetService("HttpService")
 
@@ -9,18 +9,18 @@ local image_request = syn.request({
 
 local image_url = http_service:JSONDecode(image_request.Body).data[1].imageUrl
 
+local servers, cursor = {}
 local function get_servers()
-    local servers, cursor = {}
     while true do
         local data = syn.request({
-            Url = string.format("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Asc&limit=100%s", game_id, cursor and "cursor=" .. cursor or "")
+            Url = string.format("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Desc&limit=100%s", game_id, cursor and "&cursor=" .. cursor or "")
         })
         data = http_service:JSONDecode(data.Body)
-        if not data.data then
-            break
-        end
         for _, server in pairs(data.data) do
-            table.insert(servers, server)
+            servers[#servers + 1] = {
+                server_id = server.id,
+                player_tokens = server.playerTokens
+            }
         end
         if not data.nextPageCursor then
             break
@@ -30,12 +30,14 @@ local function get_servers()
     return servers
 end
 
+rconsoleprint("gathering servers...\n")
 local servers = get_servers()
+rconsoleprint("found " .. #servers .. " servers\n")
 for _, server in pairs(servers) do
     local server_data = {}
-    for i, t in next, server.playerTokens do
+    for i = 1, #server.player_tokens do
         table.insert(server_data, {
-            token = t,
+            token = server.player_tokens[i],
             type = "AvatarHeadshot",
             size = "150x150"
         })
@@ -48,9 +50,10 @@ for _, server in pairs(servers) do
             ["Content-Type"] = "application/json"
         }
     })
+    rconsoleprint("searching server " .. server.server_id .. "\n")
     for _, v in next, http_service:JSONDecode(post_request.Body).data do
         if v.imageUrl == image_url then
-            game:GetService("TeleportService"):TeleportToPlaceInstance(game_id, server.id)
+            game:GetService("TeleportService"):TeleportToPlaceInstance(game_id, server.server_id)
         end
     end
 end
