a635e1b33c70d33aa3867315b05d4d66510fe77d
ScreamBirb <59750253+ScreamBirb@users.noreply.github.com>
2022-05-09 07:28:38 -0230

    faster speeds.

--- sniper.lua
+++ sniper.lua
@@ -1,6 +1,7 @@
-local user_id = "3515195953"
-local game_id = "3976767347"
+local user_id = "3485422903"
+local game_id = tostring(game.PlaceId)
 
+local start_tick = tick()
 local http_service = game:GetService("HttpService")
 
 local image_request = syn.request({
@@ -9,48 +10,45 @@ local image_request = syn.request({
 
 local image_url = http_service:JSONDecode(image_request.Body).data[1].imageUrl
 
-local servers, cursor = {}
-local function get_servers()
-    while true do
-        local data = syn.request({
-            Url = string.format("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Desc&limit=100%s", game_id, cursor and "&cursor=" .. cursor or "")
-        })
-        data = http_service:JSONDecode(data.Body)
-        for _, server in pairs(data.data) do
-            table.insert(servers, server)
-        end
-        if not data.nextPageCursor then
-            break
-        end
-        cursor = data.nextPageCursor
-    end
-    return servers
-end
-
-rconsoleprint("gathering servers...\n")
-local servers = get_servers()
-rconsoleprint("found " .. #servers .. " servers\n")
-for _, server in pairs(servers) do
-    local server_data = {}
-    for i = 1, #server.playerTokens do
-        table.insert(server_data, {
-            token = server.playerTokens[i],
-            type = "AvatarHeadshot",
-            size = "150x150"
-        })
-    end
-    local post_request = syn.request({
-        Url = "https://thumbnails.roblox.com/v1/batch",
-        Method = "POST",
-        Body = http_service:JSONEncode(server_data),
-        Headers = {
-            ["Content-Type"] = "application/json"
-        }
+local cursor
+while true do
+    local data = syn.request({
+        Url = string.format("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Desc&limit=100%s", game_id, cursor and "&cursor=" .. cursor or "")
     })
-    rconsoleprint("searching server " .. server.id .. "\n")
-    for _, v in next, http_service:JSONDecode(post_request.Body).data do
-        if v.imageUrl == image_url then
-            game:GetService("TeleportService"):TeleportToPlaceInstance(game_id, server.id)
-        end
+    data = http_service:JSONDecode(data.Body)
+    for _, server in pairs(data.data) do
+        task.spawn(function()
+            local server_data = {}
+            for i = 1, #server.playerTokens do
+                table.insert(server_data, {
+                    token = server.playerTokens[i],
+                    type = "AvatarHeadshot",
+                    size = "150x150"
+                })
+            end
+            local post_request = syn.request({
+                Url = "https://thumbnails.roblox.com/v1/batch",
+                Method = "POST",
+                Body = http_service:JSONEncode(server_data),
+                Headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
+            rconsoleprint("searching server " .. server.id .. "\n")
+            local post_data = http_service:JSONDecode(post_request.Body).data
+            if not post_data then
+                return
+            end
+            for _, v in next, post_data do
+                if v.imageUrl == image_url then
+                    warn("found server " .. server.id .. " in " .. math.floor(tick() - start_tick) .. " seconds\n")
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(game_id, server.id)
+                end
+            end
+        end)
     end
+    if not data.nextPageCursor then
+        break
+    end
+    cursor = data.nextPageCursor
 end
